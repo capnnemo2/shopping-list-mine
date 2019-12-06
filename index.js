561a8bfac8e72143de96b573c6bcb281b76ecf6a
Ben Hernandez <benjammin234@gmail.com>
2019-12-06 15:32:00 -0800

    attempt to get js to check/uncheck

--- index.js
+++ index.js
@@ -4,14 +4,17 @@
 // check and uncheck items on the list by clicking the "Check" button
 // permanently remove items from the list
 
+'use strict';
 
-$(function() {
-    $('#js-shopping-list-form').submit(event => {
-        event.preventDefault();
-
-        const listItem = $(event.currentTarget.find('input[name="shopping-list-entry"]'));
-
-
-
+function handleCheckUncheck() {
+    $('.shopping-item-toggle').on('click', function(e) {
+        const checked = $(this.parent('.shopping-item'));
+        if (checked.hasClass('.shopping-item_checked') {
+            $(checked).removeClass('.shopping-item_checked');
+        } else {
+            $(checked).addClass('.shopping-item_checked');
+        }
     })
-})
+}
+
+$(handleCheckUncheck);
